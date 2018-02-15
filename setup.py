4799f10f7b927ee0bda802a43e23e220007805a9
Travis Ralston <travpc@gmail.com>
2018-02-14 21:38:06 -0700

    Fix spelling error in module name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="syanpse_test_plugin",
+    name="synapse_test_plugin",
     version="0.0.1",
     description="A test plugin for synapse",
     zip_safe=False,
