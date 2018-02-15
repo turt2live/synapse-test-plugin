99945bba9cb65fff5e1d5b3a130e12de81a296ed
Travis Ralston <travpc@gmail.com>
2018-02-14 22:46:40 -0700

    Try just logging the event again

--- synapse_test_plugin/__init__.py
+++ synapse_test_plugin/__init__.py
@@ -1,21 +1,13 @@
+import logging
+
+
 class TestPlugin(object):
     def __init__(self, config, api):
         self._config = config
         self._api = api
 
     def on_room_directory_association_created(self, event):
-        self._api.hs.get_event_creation_handler().create_and_send_nonmember_event(
-            event['creator'],
-            {
-                "type": "m.room.message",
-                "room_id": event['room_id'],
-                "sender": event['creator'],
-                "content": {
-                    "msgtype": "m.notice",
-                    "body": "[Test Plugin] Alias " + event['room_alias'].to_string() + " created on this room",
-                }
-            }
-        )
+        logging.info("Room alias created: %r", event)
 
     @staticmethod
     def parse_config(config):
