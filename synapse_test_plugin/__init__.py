80b6f5414b1a10107f38265809d551cb9b0c5024
Travis Ralston <travpc@gmail.com>
2018-02-14 22:49:42 -0700

    Safer logging

--- synapse_test_plugin/__init__.py
+++ synapse_test_plugin/__init__.py
@@ -1,13 +1,16 @@
 import logging
 
 
+logger = logging.getLogger(__name__)
+
+
 class TestPlugin(object):
     def __init__(self, config, api):
         self._config = config
         self._api = api
 
     def on_room_directory_association_created(self, event):
-        logging.info("Room alias created: %r", event)
+        logger.info("Room alias created: %r", event)
 
     @staticmethod
     def parse_config(config):
