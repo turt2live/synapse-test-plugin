62e3f9557d8ea4f2c2de00470959ea701761a9c3
Travis Ralston <travpc@gmail.com>
2018-02-14 22:53:58 -0700

    Just log to the main logger for easy visibility in the synapse logs

--- synapse_test_plugin/__init__.py
+++ synapse_test_plugin/__init__.py
@@ -1,16 +1,13 @@
 import logging
 
 
-logger = logging.getLogger(__name__)
-
-
 class TestPlugin(object):
     def __init__(self, config, api):
         self._config = config
         self._api = api
 
     def on_room_directory_association_created(self, event):
-        logger.info("Room alias created: %r", event)
+        logging.info("[TEST PLUGIN] Room alias created: %r", event)
 
     @staticmethod
     def parse_config(config):
